407bcd44dc61bbcc7ec44523a47e9d083def7209
Pavel <pasha3103@bk.ru>
2017-05-24 10:15:43 +0300

    request to api

--- src/main/java/services/SportDataService.kt
+++ src/main/java/services/SportDataService.kt
@@ -6,6 +6,10 @@ import model.Country
 import model.football.Team
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 @Component
 class SportDataService() : SportData
@@ -15,7 +19,26 @@ class SportDataService() : SportData
 
     override fun getTeam(teamId: Int): Team
     {
-        throw UnsupportedOperationException()
+        val USER_AGENT = "Mozilla/5.0"
+        val url = "http://api.football-data.org/v1/competitions/426/leagueTable"
+        val obj = URL(url)
+
+        val connection = obj.openConnection() as HttpURLConnection
+
+        connection.requestMethod = "GET"
+        connection.setRequestProperty("User-Agent", USER_AGENT)
+        connection.setRequestProperty("X-Auth-Token", "b821e893b7234fe19c4e754f431b476a")
+
+        val reader = BufferedReader(InputStreamReader(connection.inputStream))
+        val response = StringBuffer()
+
+        for (inputLine in reader.readLine())
+        {
+            response.append(inputLine)
+        }
+        reader.close()
+
+        return Team("a", Country("RUSSIA"))
     }
 
     override fun getLeague(countryId: Int): Collection<Team>
