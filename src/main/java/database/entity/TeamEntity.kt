78923e73ce6872bb0b7dfbd75f0b2ce9263561df
Pavel <pasha3103@bk.ru>
2017-05-25 10:02:14 +0300

    team entity

---
+++ src/main/java/database/entity/TeamEntity.kt
@@ -0,0 +1,40 @@
+package database.entity
+
+import java.util.*
+
+class TeamEntity(id : Long)
+{
+    val Id : Long = id
+        get
+
+    var Name : String? = null
+        get
+        set
+
+    var ShortName : String? = null
+        get
+        set
+
+    var Country : CountryEntity? = null
+        get
+        set
+
+    override fun equals(other: Any?): Boolean
+    {
+        if (other == null)
+        {
+            return false
+        }
+
+        if (other !is TeamEntity)
+        {
+            return false
+        }
+        return Objects.equals(this.Id, other.Id);
+    }
+
+    override fun hashCode(): Int
+    {
+        return Objects.hashCode(Id)
+    }
+}
