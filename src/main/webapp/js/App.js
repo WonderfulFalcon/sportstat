03bffe67fce7d4c0e06c39f584a8c22c684e3687
Pavel <pasha3103@bk.ru>
2017-07-08 01:23:10 +0300

    add header component

--- src/main/webapp/js/App.js
+++ src/main/webapp/js/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <p class="logotext">
+                    Football<strong>Stat</strong>
+                </p>
+            </div>
+        )
+    }
+}
+
 class App extends Component {
     render() {
         return (
             <div>
-                <select id="leagueInfo">
-                    {this.props.testStore.map((league, index) =>
-                        <option key={index} data-league-id={league.id} data-league-tours-played={league.toursPlayed}>
-                            {league.name}
-                        </option>
-                    )}
-                </select>
+                <div>
+                    <select id="leagueInfo">
+                        {this.props.testStore.map((league, index) =>
+                                <option key={index} data-league-id={league.id} data-league-tours-played={league.toursPlayed}>
+                                    {league.name}
+                                </option>
+                        )}
+                    </select>
+                </div>
             </div>
         );
     }
