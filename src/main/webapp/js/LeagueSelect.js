edc77d957ec11314ba44eaf95f5df0ce806bb6fb
SuperUser <p@bk.ru>
2017-07-13 12:03:36 +0300

    remove tableControls.jsp

--- src/main/webapp/js/LeagueSelect.js
+++ src/main/webapp/js/LeagueSelect.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class LeagueSelect extends Component {
+    leagueChanged(event) {
+        {console.log(event)}
+    }
+
     render () {
         return (
             <div>
-                <select id="leagueInfo">
+                <select id="leagueInfo" onChange={this.leagueChanged}>
                     {this.props.testStore.map((league, index) =>
                         <option key={index} data-league-id={league.id}
                                 data-league-tours-played={league.toursPlayed}>
