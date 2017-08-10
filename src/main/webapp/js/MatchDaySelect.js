4c4dcf76087461c2ba686ebde36ecaf8d2705db6
SuperUser <p@bk.ru>
2017-08-10 12:47:32 +0300

    matchDaySelect fix

--- src/main/webapp/js/MatchDaySelect.js
+++ src/main/webapp/js/MatchDaySelect.js
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 
 class MatchDaySelect extends Component {
+    toursPlayed () {
+        const league = this.props.leagueTable;
+        return league ? league.matchDay : 0
+    }
+
+    matchDays () {
+        let matchDays = [];
+        for (var i = 1; i < this.toursPlayed() + 1; i++) {
+            matchDays.push(i);
+        }
+        return matchDays;
+    }
+
     render () {
         return (
             <div>
                 <select id="matchDay" name="Match Day">
-                    {this.props.availableLeagues.map((league, index) =>
-                        <option key={index}>
-                            {index}
-                        </option>
+                    {this.matchDays().map((item, count) =>
+                        <MatchDayOption matchday={item} />
                     )}
                 </select>
             </div>
@@ -18,7 +29,17 @@ class MatchDaySelect extends Component {
     }
 }
 
+class MatchDayOption extends Component {
+    render () {
+        return (
+            <option key={this.props.matchday}>
+                {this.props.matchday}
+            </option>
+        );
+    }
+}
+
 export default connect(
-        state => ({ availableLeagues : state.availableLeagues }),
+        state => ({ leagueTable : state.leagueTable }),
         dispatch => ({})
 )(MatchDaySelect);
