59744aeff91b42c6b48a33b87e403ff3403de675
Pavel <pasha3103@bk.ru>
2017-08-15 01:33:17 +0300

    get matches for league

--- src/main/webapp/js/LeagueInfo.js
+++ src/main/webapp/js/LeagueInfo.js
@@ -1,11 +1,50 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
 class LeagueInfo extends Component {
     render () {
         return (
-            <div id="col1"></div>
+            <div id="col1">
+                {this.props.leagueMatches.length > 0 &&
+                    <table class="match-results">
+                        <thead>
+                            <tr>
+                                <th>
+                                    <span>
+                                        Match results:
+                                    </span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <LeagueStatistic matches={this.props.leagueMatches} />
+                    </table>
+                }
+            </div>
         )
     }
 }
 
-export default LeagueInfo;
+class LeagueStatistic extends Component {
+    render () {
+        return (
+            <tbody>
+                {this.props.matches.map((match, index) =>
+                    <tr>
+                        <td><span>{match.homeTeamName}</span></td>
+                        <td class="scoreColumn">
+                            <span>${match.goalsHomeTeam}</span>
+                            <span>-</span>
+                            <span>${match.goalsAwayTeam}</span>
+                        </td>
+                        <td><span>${match.awayTeamName}</span></td>
+                    </tr>
+                )}
+            </tbody>
+        )
+    }
+}
+
+export default connect(
+    state => ({ leagueMatches : state.leagueMatches }),
+    dispatch => ({})
+);
