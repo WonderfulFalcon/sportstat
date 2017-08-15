9034e7934944385a0ae31bf91452c245e3d8a140
Sakovich Oleg <osakovich@marathonbet.ru>
2017-08-15 14:30:34 +0300

    Fix - Highlighting selected command

--- src/main/webapp/js/LeagueTable.js
+++ src/main/webapp/js/LeagueTable.js
@@ -64,14 +64,15 @@ class TableBody extends Component {
     }
 
     render () {
+        $('.teamColumn.selected').removeClass('selected');
         return (
             <tbody>
             {this.props.teams.map((team, index) =>
                 <tr key={index}>
                     <td><span>{team.statistic.position}</span></td>
-                    <td className="teamColumn" data-clickable-team={team.id} onClick={function (event) {
-                        $('.selected').removeClass('selected');
-                        $(event.target).closest('td').addClass('selected');
+                    <td className="teamColumn" data-clickable-team={team.id} onClick={function () {
+                         $('.teamColumn.selected').removeClass('selected');
+                         $('[data-clickable-team=' + team.id + ']').addClass('selected');
                         loadPlayers(team.id);
                     }}>
                         <div className="club-logo">
