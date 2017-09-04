c5abb3e6d85a4c5eec34f1b7b05ba47154410a5d
Pavel Demin <pasha3103@bk.ru>
2017-09-04 18:36:35 +0300

    decompisition team players container and component

--- src/main/webapp/js/components/TeamPlayers.js
+++ src/main/webapp/js/components/TeamPlayers.js
@@ -6,25 +6,34 @@ class TeamPlayers extends Component {
         return "flag-" + country;
     }
 
+    isShowPlayers () {
+        return !$.isEmptyObject(this.props.currentSelectedTeam) &&
+            this.props.teamPlayers.teamPlayers &&
+            this.props.teamPlayers.teamPlayers.length > 0
+    }
+
     render () {
         return (
-            <div>
-                <div className="players-table-header">
-                    <TeamName teamName={this.props.teamPlayers.teamName} />
-                </div>
-                <table>
-                    <tbody>
-                    {this.props.teamPlayers.teamPlayers.map((player, index) =>
-                        <tr key={index}>
-                            <td className="player-country">
-                                <i className={TeamPlayers.logoClass(player.country)}/>
-                            </td>
-                            <td>{player.name}</td>
-                            <td>{player.number}</td>
-                        </tr>
-                    )}
-                    </tbody>
-                </table>
+            <div id="col3">
+                {this.isShowPlayers() && <div>
+                        <div className="players-table-header">
+                            <TeamName teamName={this.props.teamPlayers.teamName} />
+                        </div>
+                        <table>
+                            <tbody>
+                                {this.props.teamPlayers.teamPlayers.map((player, index) =>
+                                    <tr key={index}>
+                                        <td className="player-country">
+                                            <i className={TeamPlayers.logoClass(player.country)}/>
+                                        </td>
+                                        <td>{player.name}</td>
+                                        <td>{player.number}</td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                }
             </div>
         )
     }
--- src/main/webapp/js/containers/TeamPlayersContainer.js
+++ src/main/webapp/js/containers/TeamPlayersContainer.js
@@ -1,28 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import TeamPlayers from './../components/TeamPlayers';
 
-class TeamPlayersContainer extends Component {
-    isShowPlayers () {
-        return !$.isEmptyObject(this.props.currentSelectedTeam) &&
-            this.props.teamPlayers.teamPlayers &&
-            this.props.teamPlayers.teamPlayers.length > 0
-    }
-
-    render () {
-        return (
-            <div id="col3">
-                {this.isShowPlayers() && <TeamPlayers teamPlayers={this.props.teamPlayers} /> }
-            </div>
-        )
-    }
-}
-
-export default connect(
-    state => ({
+const mapStateToProps = (state) => {
+    return {
         currentSelectedTeam: state.currentSelectedTeam,
         teamPlayers : state.teamPlayers
-    }),
-    dispatch => ({})
-)(TeamPlayersContainer);
+    }
+};
+
+export default connect(mapStateToProps)(TeamPlayers);
