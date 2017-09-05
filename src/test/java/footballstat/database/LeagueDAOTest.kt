d7addc0a4f7cdc9fd28a653167da2985a5e6e009
Sakovich Oleg <osakovich@marathonbet.ru>
2017-09-05 14:44:55 +0300

    init DAO layer

--- src/test/java/footballstat/database/LeagueDAOTest.kt
+++ src/test/java/footballstat/database/LeagueDAOTest.kt
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-@Transactional
 open class LeagueDAOTest {
     @Autowired
     lateinit var leagueDao : DAO<League>
